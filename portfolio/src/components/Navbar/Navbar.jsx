d7777ab0cd63f9db6b501f2aa3652686e5eef3f1
PriyanshuK2 <137306612+PriyanshuK2@users.noreply.github.com>
2025-09-06 22:45:08 +0530

    Creating Navbar

--- portfolio/src/components/Navbar/Navbar.jsx
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import { FiMenu } from "react-icons/fi";
+import { FiX } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
 
 export const Navbar = () => {
   return (
